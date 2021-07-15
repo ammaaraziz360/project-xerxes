73dece3045e7f5a8b5808db6da43ea5e17b19b20
Ammaar Mohammed <ammaaraziz360@gmail.com>
2021-07-15 00:01:49 -0500

    dev update

--- backend/flask_API/mysql_db_access/mysqldb_credentials.py
+++
@@ -1,6 +0,0 @@
-class Credentials:
-    def __init__(self, database):
-        self.HOST_NAME = 'db-xerxes.cfhinzrtpqwd.us-east-2.rds.amazonaws.com'
-        self.USER_NAME = 'admin'
-        self.USER_PASSWORD = 'EoxmZigQFfy2a62SoWWs'
-        self.database = database
